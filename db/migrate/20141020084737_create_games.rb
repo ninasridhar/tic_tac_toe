4473aa93c2771047d9349fa64416f011c82d1825
ninasridhar <nina.sridhar.1@gmail.com>
2014-10-23 16:36:29 +0100

    deleted unused/unwanted functionality generated by rails

--- db/migrate/20141020084737_create_games.rb
+++ db/migrate/20141020084737_create_games.rb
@@ -3,7 +3,6 @@ class CreateGames < ActiveRecord::Migration
     create_table :games do |t|
       t.integer :player1_id
       t.integer :player2_id
-      t.integer :winner
 
       t.timestamps
     end
